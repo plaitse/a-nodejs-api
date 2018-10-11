02867a1c5e2c123158499918182c1a30ba63fda6
Pierre Laitselart <pierre.laitselart@bigyouth.fr>
2018-10-11 17:31:00 +0200

    Commit

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+// Library imports
+const bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const { ObjectID } = require('mongodb');
+const PORT = process.env.PORT || 5000;
 
-express()
+// Local imports
+const { mongoose } = require('./server/mongoose/mongoose');
+const { Todo } = require('./server/models/todo');
+const { User } = require('./server/models/user');
+
+const app = express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
