88c6e33f73736efeaac34b446365490539e51862
Pierre Laitselart <pierre.laitselart@bigyouth.fr>
2018-10-11 17:34:52 +0200

    Commit

--- index.js
+++ index.js
@@ -14,5 +14,45 @@ const app = express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .get('/', (req, res) => res.render('pages/index'));
+
+// POST
+app.post('/todos', (req, res) => {
+  let todo = new Todo({
+      text: req.body.text
+  });
+  todo.save().then((doc) => {
+      res.status(200).send(doc);
+  }, (error) => {
+      res.status(400).send(error);
+  });
+});
+
+// GET ALL
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+      res.status(200).send({todos});
+  }, (error) => {
+      res.send(400).send(error);
+  });
+});
+
+// GET BY ID
+app.get('/todos/:id', (req, res) => {
+  let id = req.params.id;
+  if (!ObjectID.isValid(id)) {
+      return res.status(404).send('Id not valid');
+  }
+  Todo.findById(id).then((todo) => {
+      if (!todo) {
+          return res.status(404).send('Id not found');
+      }
+      res.status(200).send({todo})
+  }).catch((error) => {
+      res.status(400).send(error);
+  });
+});
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+module.exports = { app };
