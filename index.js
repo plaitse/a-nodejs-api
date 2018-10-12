d9538c3440e2485a22d17c1e110f3e2fee64532c
Pierre Laitselart <pierre.laitselart@bigyouth.fr>
2018-10-12 10:35:31 +0200

    Commit

--- index.js
+++ index.js
@@ -55,6 +55,22 @@ app.get('/todos/:id', (req, res) => {
   });
 });
 
+// DELETE BY ID
+app.delete('/todos/:id', (req, res) => {
+    let id = req.params.id;
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send('Id not valid');
+    }
+    Todo.findByOneAndRemove({_id: id}).then((todo) => {
+        if (!todo) {
+            return res.status(404).send('Id not found');
+        }
+        res.status(200).send({todo})
+    }).catch((error) => {
+        res.status(400).send(error);
+    });
+});
+
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 module.exports = { app };
