56c861c3d0bc0f028c52d164c9b8559c773a0750
Pierre Laitselart <pierre.laitselart@bigyouth.fr>
2018-10-12 10:44:17 +0200

    Commit

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ app.delete('/todos/:id', (req, res) => {
     if (!ObjectID.isValid(id)) {
         return res.status(404).send('Id not valid');
     }
-    Todo.findByOneAndRemove({_id: id}).then((todo) => {
+    Todo.findOneAndDelete({_id: id}).then((todo) => {
         if (!todo) {
             return res.status(404).send('Id not found');
         }
